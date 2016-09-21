2f5703d613954ed7d36680c0604741ff374cdd7e
Bas <basvandriel94@gmail.com>
2016-09-21 13:45:34 +0200

    Refactoring

--- src/bas/animalkingdom/animal/Animal.java
+++ src/bas/animalkingdom/animal/Animal.java
@@ -2,17 +2,37 @@ package bas.animalkingdom.animal;
 
 import bas.animalkingdom.animal.gender.Gender;
 
-public class Animal {
+public abstract class Animal {
 
     /**
      * The {@link bas.animalkingdom.animal.gender.Gender} of the {@link Animal}
      */
     protected Gender gender;
+
+    /**
+     *
+     */
     protected String bodyCovering;
+
+    /**
+     *
+     */
     protected String name;
+
+    /**
+     *
+     */
     protected String color;
+
+    /**
+     *
+     */
     protected int weight;
 
+    /**
+     *
+     * @param gender {@link Gender} adf
+     */
     public Animal(Gender gender) {
         this.gender = gender;
     }
--- src/bas/animalkingdom/animal/impl/reptile/IReptile.java
+++ src/bas/animalkingdom/animal/impl/reptile/IReptile.java
@@ -2,8 +2,5 @@ package bas.animalkingdom.animal.impl.reptile;
 
 import bas.animalkingdom.animal.IEggLayer;
 
-/**
- * Created by Bas on 20-9-2016.
- */
-public interface IReptile extends IEggLayer{
+public interface IReptile extends IEggLayer {
 }
