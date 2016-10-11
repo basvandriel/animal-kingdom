4c3cf076ed8f94c497042618a838171719aa9784
Bas <basvandriel94@gmail.com>
2016-10-11 22:17:17 +0200

    Added javadoc to the Cage and Zoo classes

--- src/bas/animalkingdom/zoo/Cage.java
+++ src/bas/animalkingdom/zoo/Cage.java
@@ -9,31 +9,59 @@ import com.sun.scenario.animation.shared.AnimationAccessor;
 
 import java.util.ArrayList;
 
+
 /**
- * Created by Bas on 20-9-2016.
+ *
  */
 public class Cage {
 
+    /**
+     * The caged {@link Animal}s in this {@link Cage}.
+     */
     private ArrayList<Animal> cagedAnimals;
 
+    /**
+     * Selects a {@link Animal} in this {@link Cage}.
+     *
+     * @return A {@link Animal} in this {@link Cage}.
+     */
     public Animal selectAnimal() {
         return new Crocodile(new Male());
     }
 
+    /**
+     * The type of this {@link Cage}.
+     */
     public void getCageType() {
+
     }
 
+    /**
+     * Adds a {@link Animal} in to {@link Cage}.
+     *
+     * @param anAnimal The {@link Animal} to add in this {@link Cage}.
+     *
+     * @return If the {@link Animal} in this {@link Cage} could be added.
+     */
     public boolean addAnimal(Animal anAnimal) {
         return false;
     }
 
+    /**
+     * Retrieves the caged {@link Animal}s in this {@link Cage}.
+     *
+     * @return The caged {@link Animal}s in this {@link Cage}.
+     */
     public ArrayList<Animal> getCagedAnimals() {
         return this.cagedAnimals;
     }
 
+    /**
+     * Adds {@link Animal} reptile eggs to this {@link Cage}.
+     *
+     * @param reptileEggs The {@link Animal} reptile eggs to this {@link Cage}.
+     */
     public void addReptileEggs(ArrayList<Egg> reptileEggs) {
 
     }
-
-
 }
--- src/bas/animalkingdom/zoo/Zoo.java
+++ src/bas/animalkingdom/zoo/Zoo.java
@@ -8,45 +8,109 @@ import java.util.ArrayList;
 import java.util.TreeSet;
 
 /**
- * Created by Bas on 20-9-2016.
+ * A {@link Zoo}
  */
 public class Zoo {
 
     /**
-     *
+     * The name of the {@link Zoo}.
      */
     private String name;
 
     /**
-     *
+     * The {@link Cage}'s in this {@link Zoo}.
      */
     private ArrayList<Cage> cages;
 
+    /**
+     * Creates a new {@link Zoo}.
+     *
+     * @param name  The name of the {@link Zoo}.
+     * @param cages The {@link Cage}'s in this {@link Zoo}.
+     */
     public Zoo(String name, ArrayList<Cage> cages) {
         this.name = name;
         this.cages = cages;
     }
 
+    /**
+     * Retrieves the name of the {@link Zoo}.
+     *
+     * @return The name of the {@link Zoo}.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Retrieves the {@link Cage}'s in this {@link Zoo}.
+     *
+     * @return The {@link Cage}'s in this {@link Zoo}.
+     */
+    public ArrayList<Cage> getCages() {
+        return cages;
+    }
+
+    /**
+     * Gets the instance of a specific {@link Zoo}.
+     *
+     * @param zoo The {@link Zoo} name.
+     *
+     * @return A specific {@link Zoo}.
+     */
     public Zoo getInstance(String zoo) {
         return this;
     }
 
+    /**
+     * Gets the instance of the current {@link Zoo}.
+     *
+     * @return The current {@link Zoo}.
+     */
     public Zoo getInstance() {
         return this;
     }
 
+    /**
+     * Adds a {@link Cage} to this {@link Zoo}.
+     *
+     * @param cage The {@link Cage} to add to the {@link Zoo}.
+     *
+     * @return If the {@link Cage} has been added to the {@link Zoo}
+     */
     public boolean addCage(Cage cage) {
         return false;
     }
 
+    /**
+     * Adds a {@link Animal} to this {@link Zoo}.
+     *
+     * @param animal The {@link Animal} to add to the {@link Zoo}.
+     *
+     * @return If the {@link Animal} has been added to the {@link Zoo}
+     */
     public boolean addAnimal(Animal animal) {
         return false;
     }
 
+    /**
+     * Retrieves the {@link Cage} of a specific {@link Animal}.
+     *
+     * @param anAnimal The {@link Animal} to retrieve the cage for.
+     *
+     * @return The {@link Cage} of a specific {@link Animal}.
+     */
     public Cage getCageOfAnimal(Animal anAnimal) {
         return new Cage();
     }
 
+    /**
+     * Retrieves the {@link Cage}s of a specific {@link Animal} by its species.
+     *
+     * @param anAnimal The {@link Animal} to retrieve the cages for.
+     *
+     * @return The {@link Cage}s of a specific {@link Animal} by its species.
+     */
     public TreeSet<Cage> getCagesBySpeciesOfAnimal(Animal anAnimal) {
         TreeSet<Cage> cages = new TreeSet<>();
         cages.add(new Cage());
@@ -54,6 +118,13 @@ public class Zoo {
         return cages;
     }
 
+    /**
+     * Retrieves the {@link Cage}s of specific {@link Animal} species.
+     *
+     * @param species The {@link Animal} species to retrieve the cages for.
+     *
+     * @return The {@link Cage}s of specific {@link Animal} species
+     */
     public TreeSet<Cage> getCageBySpecies(Class<? extends Animal> species) {
         TreeSet<Cage> cages = new TreeSet<>();
         cages.add(new Cage());
@@ -61,25 +132,56 @@ public class Zoo {
         return cages;
     }
 
+    /**
+     * Retrieves the {@link Cage} of specific {@link Animal} race.
+     *
+     * @param race The {@link Animal} raace to retrieve the cage for.
+     *
+     * @return The {@link Cage} of specific {@link Animal} race.
+     */
     public Cage getCageByRace(Class<? extends Animal> race) {
         return new Cage();
     }
 
+    /**
+     * Retrieves the {@link Animal}s of a specific {@link Animal} race.
+     *
+     * @param race The {@link Animal} race to retrieve the {@link Animal}s for.
+     *
+     * @return The {@link Animal}s of a specific {@link Animal} race.
+     */
     public ArrayList<Animal> getAllAnimalsByRace(Class<? extends Animal> race) {
         ArrayList<Animal> animals = new ArrayList<>();
         return animals;
     }
 
+    /**
+     * Retrieves the {@link Animal}s of a specific {@link Animal} species.
+     *
+     * @param species The {@link Animal} species to retrieve the {@link Animal}s for.
+     *
+     * @return The {@link Animal}s of a specific {@link Animal} species.
+     */
     public ArrayList<Animal> getAllAnimalsBySpecies(Class<? extends Animal> species) {
         ArrayList<Animal> animals = new ArrayList<>();
         return animals;
     }
 
+    /**
+     * Retrieves all the {@link Animal}s.
+     *
+     * @return All the {@link Animal}s.
+     */
     public ArrayList<Animal> getAllAnimals() {
         ArrayList<Animal> animals = new ArrayList<>();
         return animals;
     }
 
+    /**
+     * Adds {@link Animal} eggs.
+     *
+     * @param reptileEggs The {@link Animal} eggs to add
+     */
     public void addEggsOfReptiles(ArrayList<Egg> reptileEggs) {
 
     }
